dc9179d28116caabbf46b1403e02e80995bd19cc
arihant1805 <22cs3016@rgipt.ac.in>
2025-05-29 17:19:00 +0530

    changed supervisor name

--- orchestrator/supervisor.py
+++ orchestrator/supervisor.py
@@ -32,6 +32,7 @@ def get_supervisor():
         ),
         add_handoff_back_messages=True,
         output_mode="full_history",
+        supervisor_name = 'Multi source Multi agent finance assistant' 
     ).compile()
 
 # supervisor = get_supervisor()
